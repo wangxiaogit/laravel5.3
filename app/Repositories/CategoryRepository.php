eaefd0f318f8fa418bcc0660dbcf67cda1c85e6f
wangxiao <569697210@qq.com>
2017-04-17 18:10:26 +0800

    add logical data

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/17
+ * Time: 15:57
+ */
+
+namespace App\Repositories;
+
+
+class CategoryRepository
+{
+
+}
---
+++ app/Repositories/TagRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/17
+ * Time: 14:45
+ */
+
+namespace App\Repositories;
+
+
+class TagRepository
+{
+
+}
